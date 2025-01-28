3d12a5f6fe146ebc3572f885b0fe5597d4b533a1
Pavlo <pavdemchnko@gmail.com>
2025-01-27 19:50:25 -0500

    Minor fixes2

---
+++ ConsoleApp/eduHillel_HW8_Arrays2/Program.cs
@@ -0,0 +1,49 @@
+﻿// 2.Визначити, чи вірно, що сума елементів масиву з пункту 1 є невід'ємне число./
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace eduHillel_HW8_Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-10, 11);
+            }
+
+            Console.WriteLine("Array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Index {i}: {numbers[i]}");
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine($"Summ of elemets: {sum}");
+
+            if (sum >= 0)
+            {
+                Console.WriteLine("Summ is positive.");
+            }
+            else
+            {
+                Console.WriteLine("Summ is negative.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/eduHillel_HW8_Arrays3/Program.cs
@@ -0,0 +1,40 @@
+﻿//3. Створити та заповнити двовимірний масив розміру 9х9 з результатами таблиці множення (у першому рядку мають бути записані добутки кожного з чисел від 1 до 9 на 1, у другому – на 2, ..., в останньому – на 9). Тобто в 1 строчці будуть значення від 1 до 9, а у другому 2,4,6,....,18 і т.д.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace eduHillel_HW8_Arrays3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row;
+            int column;
+
+            int[,] table = new int[9, 9];
+            for (row = 0; row < 9; row++)
+            {
+                for (column = 0; column < 9; column++)
+                {
+                    table[row, column] = (row + 1) * (column + 1);
+                }
+            }
+
+            Console.WriteLine("Multiplication table:");
+            for (row = 0; row < 9; row++)
+            {
+                for (column = 0; column < 9; column++)
+                {
+                    Console.Write(table[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
