d38002f447523605ea1f10ee8bef9494b3a94516
Pavlo <pavdemchnko@gmail.com>
2025-01-27 21:03:18 -0500

    Added 5 tasks

---
+++ eduHillel_HW8_Arrays4/Program.cs
@@ -0,0 +1,70 @@
+﻿//4.Створити двовимірний масив 5х5. Заповнити його будь-якими числами. Визначити та вивести на екран: а) максимальний елемент масиву; б) мінімальний елемент масиву; в) координати мінімального елемента масиву. г) координати максимального елемента масиву.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace eduHillel_HW8_Arrays4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row;
+            int column;
+            int[,] array = new int[5, 5];
+            int minRow = 0;
+            int maxRow = 0;
+            int minColumn = 0;
+            int maxColumn = 0;
+            int min = array[0, 0];
+            int max = array[0, 0];
+
+
+            Random random = new Random();
+            for (row = 0; row < 5; row++)
+            {
+                for (column = 0; column < 5; column++)
+                {
+                    array[row, column] = random.Next(-123, 123);
+                }
+            }
+            Console.WriteLine("Array:");
+            for (row = 0; row < 5; row++)
+            {
+                for (column = 0; column < 5; column++)
+                {
+                    Console.Write(array[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            for (row = 0; row < 5; row++)
+            {
+                for (column = 0; column < 5; column++)
+                {
+                    if (array[row, column] > max)
+                    {
+                        max = array[row, column];
+                        maxRow = row;
+                        maxColumn = column;
+                    }
+                    if (array[row, column] < min)
+                    {
+                        min = array[row, column];
+                        minRow = row;
+                        minColumn = column;
+                    }
+                }
+            }
+            Console.WriteLine($"Max element: {max}");
+            Console.WriteLine($"Min element: {min}");
+            Console.WriteLine($"Min element coordinates: {minRow}, {minColumn}");
+            Console.WriteLine($"Max element coordinates: {maxRow}, {maxColumn}");
+        }
+    }
+}
---
+++ eduHillel_HW8_Arrays5/Program.cs
@@ -0,0 +1,45 @@
+﻿//5.За допомогою enum створити програму, що буде запитувати у користувача кількість днів, а потім рахувати який буде день через введену кількість, якщо рахувати від понеділка і виводити результат.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace eduHillel_HW8_Arrays5
+{
+    class Program
+    {
+        enum DayOfWeek
+        {
+            Sunday = 1,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Enter the number of days:");
+            int findDay;
+
+            while (!int.TryParse(Console.ReadLine(), out findDay) || findDay < 0)
+            {
+                Console.WriteLine("Enter a valid number.");
+            }
+
+            DayOfWeek startingDay = DayOfWeek.Sunday;
+            DayOfWeek resultDay = (DayOfWeek)(((int)startingDay + findDay - 1) % 7 + 1);
+            Console.WriteLine($"In {findDay} days it will be: {resultDay}");
+        }
+
+
+    }
+    
+}
